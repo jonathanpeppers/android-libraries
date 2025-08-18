1b2700cc6c3fb883d4dc9a435ba4341f0efaa4ac
Matthew Leibowitz <mattleibow@live.com>
2025-08-18 17:47:48 +0200

    Add the `AICore` artifact (#1256)
    * Add AICore library
    * Add AICore license:
      * https://developer.android.com/ai/gemini-nano/experimental/terms

---
+++ source/com.google.ai.edge.aicore/aicore/Additions/Additions.cs
@@ -0,0 +1,19 @@
+namespace Google.AI.Edge.AICore;
+
+partial class Content
+{
+    partial class Builder
+    {
+        public global::Google.AI.Edge.AICore.Content.Builder SetParts(global::System.Collections.Generic.IList<global::Google.AI.Edge.AICore.IPart> parts)
+        {
+            Parts = parts;
+            return this;
+        }
+
+        public global::Google.AI.Edge.AICore.Content.Builder SetRole(global::Google.AI.Edge.AICore.ContentRole role)
+        {
+            Role = role;
+            return this;
+        }
+    }
+}
